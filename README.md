4cb7e36562853ee7339e9cce640d993a79e0e778
MaliaPagley <maliapagley@gmail.com>
2023-08-07 11:03:42 -0400

    Added Starter Files/ Styling
    Added:
    Components
    CSS Styling
    Dice Numbers

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-TENZI ia a game that gives the player 10 dice. The object of the game is to role the dice until all the dice are the same number.
+Tenzies is a game that provides the player with 10 dice. The objective of the game is to roll the dice until all of them show the same number.
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import React from "react"
+import Die from './Die'
 
 
 
@@ -6,10 +7,20 @@ function App() {
 
 
   return (
-    <>
-     
-       
-    </>
+   <main>
+      <div className ="dice-container">
+        <Die value="1" />
+        <Die value="2" />
+        <Die value="3" />
+        <Die value="4" />
+        <Die value="5" />
+        <Die value="6" />
+        <Die value="7" />
+        <Die value="8" />
+        <Die value="9" />
+        <Die value="10" />
+      </div>
+   </main>
   )
 }
 
